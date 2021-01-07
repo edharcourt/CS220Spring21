bf026edcdae43f0dfca5ecf7baa44d4f26fb5c25
Ed Harcourt <edharcourt@gmail.com>
2021-01-07 11:56:21 -0500

    Reorganize directory and add work for Jan 8

---
+++ jan8/netflix_bandwidth.c
@@ -0,0 +1,10 @@
+/*
+ * According to Netflix, an HD Movie uses 
+ * about 3GiB per hour. Ultra HD requires about  
+ * 7GiB per hour.  How fast does your internet 
+ * connection need to be to watch an HD movie? 
+ * How about an Ultra HD movie? 
+ * 
+ * Write a C program that prints these values 
+ * neatly formatted in mbps.
+ */ 
