b8be0d0ce647295ed0d57631d6f2a3e420db78dc
Ed Harcourt <edharcourt@gmail.com>
2021-01-24 21:30:47 -0500

    .

---
+++ jan22_pointer_quiz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void avg_sum(double a[], int n, double *avg, double *sum) {
+    *sum = 0.0;
+    for (int i = 0; i < n; i++)
+        *sum += a[i];
+    *avg = *sum / n;
+}
+
+int main() {
+    double grades[] = {98.2, 78.9, 99.0, 83.6};
+    double mean, total;
+    avg_sum(grades, 4, &mean, &total);
+    printf("mean = %lf, sum = %lf\n", mean, total);
+}
