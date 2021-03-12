d78eca8e723b1309990873ff05d7efdb73212459
Ed Harcourt <edharcourt@stlawu.edu>
2021-03-12 14:32:48 -0500

    .

--- bigint/bigint2bin.py
+++ bigint/bigint2bin.py
@@ -2,17 +2,43 @@ from typing import Tuple
 
 # takes string s and return the quotient and the remainder
 # after dividing by 2 using the long division algorithm
-def div2(s: str) -> Tuple[str,int]:
+def div2(s: str) -> Tuple[str,int, bool]:
 
     q = ""   # build up quotient from empty string
-
+    zero = True
     r = ord(s[0]) - ord('0')   # most significant digit 
 
     for i in range(len(s)):
         p = r//2   # p is partial quotient
 
+        if p != 0:
+            zero = False
+
         q = q + chr(p + ord('0'))  # build up quotient
 
         # out of bounds error
-        r = (r - 2*p) * 10 + (ord(s[i+1]) - ord('0')) 
+        if i < len(s) - 1: 
+            r = (r - 2*p) * 10 + (ord(s[i+1]) - ord('0')) 
         
+    r = r - 2*p
+    return (q,r,zero)
+
+
+def dec2bin(q: str) -> str:
+    digs = "" 
+
+    while True:
+        (q, r, z) = div2(q)
+        digs = chr(r + ord('0')) + digs  # str(r) 
+
+        # check if quotient is all zeros
+        if z:
+            break
+
+    return digs 
+
+if __name__ == "__main__":
+    s = input("Enter a number:")
+    print('0b' + dec2bin(s))
+    print(bin(int(s)))
+    
