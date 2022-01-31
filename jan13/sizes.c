36bc18a05a5711fbe1e59689f9aa8538e8787c24
Ed Harcourt <edharcourt@stlawu.edu>
2022-01-31 08:27:15 -0500

    .

--- jan13/sizes.c
+++ jan13/sizes.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("%d\n", sizeof(unsigned char));
-    printf("%d\n", sizeof(unsigned short));
-    printf("%d\n", sizeof(unsigned int));
-    printf("%d\n", sizeof(unsigned long));
-    printf("%d\n", sizeof(unsigned long long));
-    printf("%d\n", sizeof(float));
-    printf("%d\n", sizeof(double));
+    printf("uchar         %d\n", sizeof(unsigned char));
+    printf("ushort        %d\n", sizeof(unsigned short));
+    printf("uint          %d\n", sizeof(unsigned int));
+    printf("ulong         %d\n", sizeof(unsigned long));
+    printf("u long long   %d\n", sizeof(unsigned long long));
+    printf("float         %d\n", sizeof(float));
+    printf("double        %d\n", sizeof(double));
 }
